6857ba06e012728bb6cfa19176bb9dcd90039e1c
subhasish333 <48664775+subhasish333@users.noreply.github.com>
2020-05-01 03:28:36 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var constraints = { video: { facingMode: "user" }, audio: false };
-// Define constants
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
     cameraTrigger = document.querySelector("#camera--trigger")
-// Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
         .getUserMedia(constraints)
@@ -16,13 +14,11 @@ function cameraStart() {
         console.error("Oops. Something is broken.", error);
     });
 }
-// Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
+    cameraOutput.classList.asdd("taken");
 };
-// Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
